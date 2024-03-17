5337746a8116d2095310c4cda27116b1582c4ba6
riteshkhatri08 <riteshkhatri1512@gmail.com>
2024-03-18 02:10:24 +0530

    installed dotenv to use properties from .env file

--- fullstackapp/backend/server.js
+++ fullstackapp/backend/server.js
@@ -2,16 +2,20 @@ const express = require("express");
 // create express app instance
 const app = express();
 
+// Load properties from .env file
+require("dotenv").config();
+
 // define app port
-const PORT = 3000;
+const PORT = process.env.PORT;
+
 //add a  GET request mapping
 app.get("/hello", (req, res) => {
   console.log(new Date(), "INFO - Served request at /hello");
   res.send("Hey there !");
 });
+
 // start app to listen for requests on port 3000
 app.listen(PORT, () => {
   // invoke this callback function when app is ready to listen on input port and hostname
-  console.log(`Express is listening on address - http://localhost:${PORT}`);
+  console.log(`Express App is listening on address - http://localhost:${PORT}`);
 });
- 
